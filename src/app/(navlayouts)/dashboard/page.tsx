07cffca9ea90bc7eff92c696e8406c8d1e69af38
Rajatuser <rajat8146925479@gmail.com>
2024-02-16 17:13:33 +0530

    updates

--- src/app/(navlayouts)/dashboard/page.tsx
+++ src/app/(navlayouts)/dashboard/page.tsx
@@ -1,110 +1,108 @@
-import React from "react"
+'use client'
+import React, { useEffect, useState } from "react"
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Skeleton, Card, Spinner, Pagination, Chip } from "@nextui-org/react";
+import axios from 'axios';
+import { API_ROUTES } from "@/api_routes/routes";
+import { getServerCookies } from "@/app/getServerCookies";
 
-export default function Dashboard(){
 
-    return(
-        <>
-        <div className="p-4 sm:ml-64">
-            <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-                  <div className="grid grid-cols-3 gap-4 mb-4">
-                     <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                           <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                           </svg>
-                        </p>
-                     </div>
-                     <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                           <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                           </svg>
-                        </p>
-                     </div>
-                     <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                           <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                           </svg>
-                        </p>
-                     </div>
-                  </div>
-                  <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                     <p className="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                        </svg>
-                     </p>
-                  </div>
-                  <div className="grid grid-cols-2 gap-4 mb-4">
-                     <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                           <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                           </svg>
-                        </p>
-                     </div>
-                     <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                           <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                           </svg>
-                        </p>
-                     </div>
-                     <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                           <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                           </svg>
-                        </p>
-                     </div>
-                     <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                           <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                           </svg>
-                        </p>
-                     </div>
-                  </div>
-                  <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                     <p className="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                        </svg>
-                     </p>
-                  </div>
-                  <div className="grid grid-cols-2 gap-4">
-                     <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                           <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                           </svg>
-                        </p>
-                     </div>
-                     <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                           <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                           </svg>
-                        </p>
-                     </div>
-                     <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                           <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                           </svg>
-                        </p>
-                     </div>
-                     <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                           <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-                           </svg>
-                        </p>
-                     </div>
-                  </div>
-               </div>
+export default function Dashboard() {
+   const [columnsvals, setColumnvals] = useState<string[]>([])
+   const [rows, setRows] = useState<string[]>([])
+   const [loader, setLoader] = useState(true)
+
+   const fetchUsers = (token: string) => {
+      axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}${API_ROUTES.get_all_users}`, {
+         headers: {
+            'Authorization': `Bearer ${token}`
+         }
+      }).then(res => {
+         const response = res.data
+         console.log(response.data)
+         setColumnvals(Object.keys(response?.data[0]))
+         setRows(response.data)
+         setLoader(false)
+      }).catch(err => {
+         // const error = err.response.data.message
+      })
+   }
+
+   const [page, setPage] = React.useState(1);
+   const rowsPerPage = 15;
+
+   const pages = Math.ceil(rows.length / rowsPerPage);
+
+   const items = React.useMemo(() => {
+      const start = (page - 1) * rowsPerPage;
+      const end = start + rowsPerPage;
+
+      return rows.slice(start, end);
+   }, [page, rows]);
+
+
+   useEffect(() => {
+      const getToken = async () => {
+         const token = await getServerCookies('token')
+         if (token !== undefined)
+            fetchUsers(token.value)
+      }
+      getToken()
+
+   }, [])
+
+   return (
+      <>
+         <div className="p-4 sm:ml-64">
+            <div className="p-4 rounded-lg dark:border-gray-700 mt-14">
+               {
+                  loader ? <Card className="w-full space-y-5 p-4" radius="lg"><Skeleton className="w-full rounded-lg">
+                     <div className="h-9 w-4/5 rounded-lg bg-default-200"></div>
+                  </Skeleton> <Spinner /></Card> :
+                     <Table aria-label="Example static collection table" bottomContent={
+                        <div className="flex w-full justify-center">
+                           <Pagination
+                              isCompact
+                              showControls
+                              showShadow
+                              color="secondary"
+                              page={page}
+                              total={pages}
+                              onChange={(page) => setPage(page)}
+                           />
+                        </div>
+                     }
+                        classNames={{
+                           wrapper: "min-h-[222px]",
+                        }}>
+                        <TableHeader>
+                           {
+                              columnsvals.length > 0 ? columnsvals.map((val, index) => (
+                                 <TableColumn key={index}>{val}</TableColumn>
+                              )) : <Skeleton className="w-4/5 rounded-lg">
+                                 <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
+                              </Skeleton>
+                           }
+
+                        </TableHeader>
+                        <TableBody items={items}>
+                           {(item: any) => (
+                              <TableRow key={item.id}>
+                                 <TableCell>{item.id}</TableCell>
+                                 <TableCell>{item.email}</TableCell>
+                                 <TableCell>{item.role}</TableCell>
+                                 <TableCell>
+                                    <Chip className="capitalize" color={JSON.stringify(item.active) == 'true' ? 'success' : 'warning'} size="sm" variant="flat">
+                                       {JSON.stringify(item.active) == 'true' ? 'Active' : 'Inactive'}
+                                    </Chip></TableCell>
+                              </TableRow>
+                           )}
+                        </TableBody>
+                     </Table>
+
+               }
+
             </div>
-        </>
-    )
+         </div>
+      </>
+   )
 }
--- src/components/roles/roles.js
+++ src/components/roles/roles.js
@@ -1,2 +1,2 @@
 import React from "react";
-export const Roles = [{"label":"User","value":"User"},{"label":"Admin","value":"Admin"}]
+export const Roles = [{"label":"User","value":"user"},{"label":"Admin","value":"admin"}]
